69c4f04158debdc149070fafafa1b0a9c4f2e1bf
Ethan <167151595+Ethan-Mulwee@users.noreply.github.com>
2024-11-21 18:11:20 +0000

    improved board drawing and added line clearing

--- app.cpp
+++ app.cpp
@@ -1,6 +1,7 @@
 // STL
 #include <iostream>
 #include <chrono>
+#include <string>
 
 #include <raylib.h>
 
@@ -12,7 +13,9 @@ const Color colors[10] = {Color{0,0,0,0}, LIGHTGRAY, YELLOW, BLUE, RED, GREEN, O
 const int width = 12;
 const int height = 21;
 char board[width][height];
-const int renderScale = 20;
+const int boardOffsetX = 0;
+const int boardOffsetY = 1;
+const int renderScale = 25;
 
 
 // Tetromino functions
@@ -32,7 +35,7 @@ Tetromino RotateTetromino(Tetromino tetromino, int times) {
 void DrawTetromino(Tetromino tetromino, int x, int y) {
   for (int j = 0; j < 4; j++) {
     for (int i = 0; i < 4; i++) {
-      DrawRectangle((i+x)*renderScale,(j+y)*renderScale,renderScale,renderScale,colors[tetromino.shape[i][j]]);
+      DrawRectangle((i+x+boardOffsetX)*renderScale,(j+y+boardOffsetY)*renderScale,renderScale,renderScale,colors[tetromino.shape[i][j]]);
     }
   }
 }
@@ -67,6 +70,25 @@ void PlaceTetromino(Tetromino tetromino, int x, int y) {
   }
 }
 
+// Board functions
+bool CheckLine(int y) {
+  bool lineFilled = true;
+  for (int x = 1; x < width-1; x++) {
+    lineFilled = lineFilled && (board[x][y] != 0);
+  }
+  return lineFilled;
+}
+
+void ClearLine(int clearedY) {
+  for (int x = 1; x < width-1; x++) {
+    board[x][clearedY] = 0;
+    for (int y = clearedY-1; y >= 0; y--) {
+      board[x][y+1] = board[x][y];
+      board[x][y] = 0;
+    }
+  }
+}
+
 App::App(int windowWidth, int windowHeight) {
   InitWindow(windowWidth,windowHeight, "Tetris");
   SetTargetFPS(60);
@@ -76,6 +98,8 @@ App::App(int windowWidth, int windowHeight) {
 
   int tetrominoX = 6;
   int tetrominoY = 0;
+
+  int score;
 
   // Timing
   std::chrono::steady_clock clock;
@@ -138,15 +162,14 @@ App::App(int windowWidth, int windowHeight) {
         }
       }
       // Check line clears
-      bool markedLines[height];
+      bool markedLines[height-1] = {};
       for (int j = 0; j < height-1; j++) {
-        bool lineFilled = true;
-        for (int i = 1; i < width-1; i++) {
-          lineFilled = lineFilled && (board[i][j] != 0);
+        markedLines[j] = CheckLine(j);
+      }
+      for (int line = 0; line < height-1; line++) {
+        if (markedLines[line]) {
+          ClearLine(line);
         }
-        markedLines[j] = lineFilled;
-        if (lineFilled)
-          std::cout << "you filled a line!" << "\n";
       }
       GameTick = false;
     }
@@ -155,12 +178,35 @@ App::App(int windowWidth, int windowHeight) {
     BeginDrawing();
       ClearBackground(Color{31,31,31,255});
 
-      // Draw board
-      for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
-          DrawRectangle(x*renderScale,y*renderScale,renderScale,renderScale,colors[board[x][y]]);
+      // Draw UI
+
+      int LeftX = (1+boardOffsetX)*renderScale;
+      int RightX = (width-1+boardOffsetX)*renderScale;
+      int BottomY = (height-1+boardOffsetY)*renderScale;
+      int TopY = (boardOffsetY)*renderScale;
+
+
+      DrawText("score: ", RightX+20, TopY, 50, WHITE);
+
+      // Draw board background
+      DrawRectangle(LeftX, TopY, RightX - LeftX, BottomY - TopY, Color{20,20,20,255});
+
+      // Draw placed tetrominoes
+      for (int y = 0; y < height-1; y++) {
+        for (int x = 1; x < width-1; x++) {
+          DrawRectangle((x+boardOffsetX)*renderScale,(y+boardOffsetY)*renderScale,renderScale,renderScale,colors[board[x][y]]);
         }
       }
+      // Draw board borders
+      
+      // Right vertical
+      DrawLine(RightX, TopY, RightX, BottomY, WHITE);
+      // Left vertical
+      DrawLine(LeftX, TopY, LeftX, BottomY, WHITE);
+      // Bottom horizontal
+      DrawLine(LeftX, BottomY, RightX, BottomY, WHITE);
+      // Top horizontal
+      DrawLine(LeftX, TopY, RightX, TopY, WHITE);
 
       DrawTetromino(tetromino, tetrominoX, tetrominoY);
 
