014199ecb6682e68ee3eeede3971949804d0f6d7
Ethan <167151595+Ethan-Mulwee@users.noreply.github.com>
2024-11-25 18:05:09 +0000

    random move tetris AI

--- ai.cpp
+++ ai.cpp
@@ -1,10 +1,21 @@
 #include "ai.hpp"
 
 void TetrisAI::Update() {
-  if (GetRandomValue(0,1)) {
-    game->Place();
-  }
-  else {
-    game->Move(1,0);
+  auto currentUpdateTime = clock.now();
+  deltaTime = currentUpdateTime - lastUpdateTime;
+  lastUpdateTime = currentUpdateTime;
+  logicTimer += deltaTime;
+  if (logicTimer.count() > 0.5f) {
+    switch(GetRandomValue(0,3)) {
+      case 0: 
+        game->Tick(); break;
+      case 1: 
+        game->Move(1,0); break;
+      case 2: 
+        game->Move(-1,0); break;
+      case 3: 
+        game->Rotate(1); break;
+    }
+    logicTimer = std::chrono::duration<double>();
   }
 }
